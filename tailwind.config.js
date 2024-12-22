f778487ce597bda1ea4567a2726b1463b250f490
Matthew PIatek <81636184+MatthewPiatek21@users.noreply.github.com>
2024-12-22 04:27:57 -0600

    Reset back to original 2

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,14 +6,7 @@ module.exports = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {
-      keyframes: {
-        blink: {
-          '0%, 100%': { opacity: 1 },
-          '50%': { opacity: 0 }
-        }
-      }
-    }
+    extend: {}
   },
   plugins: [],
 } 
